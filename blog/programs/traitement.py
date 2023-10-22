ac5bea5141653faf267b6274915acc745667c153
fabienutt <fabien.gautier@utt.Fr>
2023-10-22 14:52:09 +0200

    debut du back
    ajout de qq fonctions pour commencer a traiter le texte d'entrée, il faut ajouter des elements a l'entrée pour pouvoir  avoir plus de precision sur le robot

--- blog/programs/traitement.py
+++ blog/programs/traitement.py
@@ -1,4 +1,29 @@
+#Traitement des données : 
+    # Mots clés à récupérer dans le texte et enregistrer le texte dans un repertoire une fois validé pour pouvoir avoir un corpus à fournir à la base de données
+    # Algo a faire dans une autre fonction
+    # Retourner les resultats du choix des composants / modele 3D / le reste 
+from datetime import datetime
+
+def getdate():
+    # Récupération de la date et de l'heure actuelles
+    now = datetime.now()
+    # Conversion en string avec le format souhaité
+    formatted_date = now.strftime('%Y%m%d%H%M%S')
+    return formatted_date
+
 def process_data(data):
     result=data
-    # Traitement des données...
+    liste=result.split(" ")
+    for elt in liste:
+        if len(elt)<4:
+            liste.remove(elt)
+    result=""
+    for elt in liste:
+        result+=elt+'; '
+    #sauvegarde du prompt
+    with open('blog/programs/datas/'+getdate()+'.txt','w' )as file :
+        file.write(result)
     return result
+
+      
+    
