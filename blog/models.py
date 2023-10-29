b2ff3ec9526daaa33668d47dc37c7b757269ece9
Kléden <kleden.rousseau@gmail.com>
2023-10-29 12:39:49 +0100

    component, assembly, complage
    pour le backend, déclaration des classes component, assembly et complage avec commentaires

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,36 @@ class Formulaire(models.Model):
     def __str__(self):
         return self.Texte
 
+
+#---database--by--kl--(et--surtout--ChatGPT)-----------------------------
+#Représente les composants individuels.
+class component(models.Model):
+    nom = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    fabricant = models.CharField(max_length=200, blank=True, null=True)
+    # Vous pouvez ajouter d'autres champs pertinents ici.
+
+    def __str__(self):
+        return self.nom
+
+#Représente les assemblages, qui sont des collections de composants.
+class assembly(models.Model):
+    nom = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    composants = models.ManyToManyField(component, through='complage')
+    # Le champ `composants` est une relation many-to-many vers le modèle Composant.
+
+    def __str__(self):
+        return self.nom
+    
+#Table de jonction représente la relation entre un composant et un assemblage. 
+#Elle peut également contenir des informations supplémentaires sur la relation, par exemple,
+#la quantité de chaque composant dans un assemblage.
+class complage(models.Model):
+    composant = models.ForeignKey(component, on_delete=models.CASCADE)
+    assemblage = models.ForeignKey(assembly, on_delete=models.CASCADE)
+    quantite = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ['composant', 'assemblage']
+        
